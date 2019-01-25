6f3f8ce4afd08f78bf359ea8d16259b112d73453
Josh Black <josh@josh.black>
2019-01-25 14:37:49 -0600

    feat(carbon-layout): enable additional replacements

--- src/migrations/carbon-layout/0.0.1-alpha.31.js
+++ src/migrations/carbon-layout/0.0.1-alpha.31.js
@@ -32,67 +32,66 @@ function createVariableRegex(name) {
 const TARGET_VERSION = '0.0.1-alpha.31';
 const changes = [
   // Breakpoints
-  // {
-  // filename: '_breakpoint.scss',
-  // from: '$grid-cell-padding',
-  // to: '$carbon--grid-cell-padding',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: '$grid-gutter',
-  // to: '$carbon--grid-gutter',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: '$grid-breakpoints',
-  // to: '$carbon--grid-breakpoints',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: /(?=breakpoint-next\(.+\))(breakpoint-next)/g,
-  // to: 'carbon--breakpoint-next',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: /(?=breakpoint-prev\(.+\))(breakpoint-prev)/g,
-  // to: 'carbon--breakpoint-prev',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: /(?=is-smallest-breakpoint\(.+\))(is-smallest-breakpoint)/g,
-  // to: 'carbon--is-smallest-breakpoint',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: /(?=largest-breakpoint-name\(.+\))(largest-breakpoint-name)/g,
-  // to: 'carbon--largest-breakpoint-name',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: 'breakpoint-infix',
-  // from: /(?=breakpoint-infix\(.+\))(breakpoint-infix)/g,
-  // to: 'carbon--breakpoint-infix',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: /(?=breakpoint-up\(.+\))(breakpoint-up)/g,
-  // to: 'carbon--breakpoint-up',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: /(?=breakpoint-down\(.+\))(breakpoint-down)/g,
-  // to: 'carbon--breakpoint-down',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: /(?=breakpoint-between\(.+\))(breakpoint-between)/g,
-  // to: 'carbon--breakpoint-between',
-  // },
-  // {
-  // filename: '_breakpoint.scss',
-  // from: /(?=breakpoint\(.+\))(breakpoint)/g,
-  // to: 'carbon--breakpoint',
-  // },
+  {
+    filename: '_breakpoint.scss',
+    from: createVariableRegex('grid-cell-padding'),
+    to: '$carbon--grid-cell-padding',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createVariableRegex('grid-gutter'),
+    to: '$carbon--grid-gutter',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createVariableRegex('grid-breakpoints'),
+    to: '$carbon--grid-breakpoints',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('breakpoint-next'),
+    to: 'carbon--breakpoint-next',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('breakpoint-prev'),
+    to: 'carbon--breakpoint-prev',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('is-smallest-breakpoint'),
+    to: 'carbon--is-smallest-breakpoint',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('largest-breakpoint-name'),
+    to: 'carbon--largest-breakpoint-name',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('breakpoint-infix'),
+    to: 'carbon--breakpoint-infix',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('breakpoint-up'),
+    to: 'carbon--breakpoint-up',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('breakpoint-down'),
+    to: 'carbon--breakpoint-down',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('breakpoint-between'),
+    to: 'carbon--breakpoint-between',
+  },
+  {
+    filename: '_breakpoint.scss',
+    from: createFunctionRegex('breakpoint'),
+    to: 'carbon--breakpoint',
+  },
 
   // Conversion
   {
@@ -112,55 +111,58 @@ const changes = [
     to: 'carbon--em',
   },
 
-  // // Key heights
-  // {
-  // filename: '_key-height.scss',
-  // from: /(?=(get-column-width\((.?)+\)))(?<!carbon--)(get-column-width)/g,
-  // to: 'carbon--get-column-width',
-  // },
-  // {
-  // filename: '_key-height.scss',
-  // from: '$key-height-scales',
-  // to: '$carbon--key-height-scales',
-  // },
-  // {
-  // filename: '_key-height.scss',
-  // from: /(?=key-height\(.+\))(key-height)/g,
-  // to: 'carbon--key-height',
-  // },
-  // // Mini-units
-  // {
-  // filename: '_mini-unit.scss',
-  // from: '$mini-unit-size',
-  // to: '$carbon--mini-unit-size',
-  // },
-  // {
-  // filename: '_mini-unit.scss',
-  // from: /(?=mini-unit\(.+\))(mini-unit)/g,
-  // to: 'carbon--mini-unit',
-  // },
-  // // Spacing
-  // {
-  // filename: '_spacing.scss',
-  // from: '$fixed-spacing-scale',
-  // to: '$carbon--fixed-spacing-scale',
-  // },
-  // {
-  // filename: '_spacing.scss',
-  // from: '$fluid-spacing-scale',
-  // to: '$carbon--fluid-spacing-scale',
-  // },
-  // {
-  // filename: '_mini-unit.scss',
-  // from: /(?=spacing\(.+\))(spacing)/g,
-  // to: 'carbon--spacing',
-  // },
-  // // Utilities
-  // {
-  // filename: '_utilities.scss',
-  // from: /(?=key-by-index\(.+\))(key-by-index)/g,
-  // to: 'carbon--key-by-index',
-  // },
+  // Key heights
+  {
+    filename: '_key-height.scss',
+    from: createFunctionRegex('get-column-width'),
+    to: 'carbon--get-column-width',
+  },
+  {
+    filename: '_key-height.scss',
+    from: createVariableRegex('key-height-scales'),
+    to: '$carbon--key-height-scales',
+  },
+  {
+    filename: '_key-height.scss',
+    from: createFunctionRegex('key-height'),
+    to: 'carbon--key-height',
+  },
+
+  // Mini-units
+  {
+    filename: '_mini-unit.scss',
+    from: createVariableRegex('mini-unit-size'),
+    to: '$carbon--mini-unit-size',
+  },
+  {
+    filename: '_mini-unit.scss',
+    from: createFunctionRegex('mini-units'),
+    to: 'carbon--mini-units',
+  },
+
+  // Spacing
+  {
+    filename: '_spacing.scss',
+    from: createVariableRegex('fixed-spacing-scale'),
+    to: '$carbon--fixed-spacing-scale',
+  },
+  {
+    filename: '_spacing.scss',
+    from: createVariableRegex('fluid-spacing-scale'),
+    to: '$carbon--fluid-spacing-scale',
+  },
+  {
+    filename: '_mini-unit.scss',
+    from: createFunctionRegex('spacing'),
+    to: 'carbon--spacing',
+  },
+
+  // Utilities
+  {
+    filename: '_utilities.scss',
+    from: createFunctionRegex('key-by-index'),
+    to: 'carbon--key-by-index',
+  },
 ];
 
 module.exports = {
